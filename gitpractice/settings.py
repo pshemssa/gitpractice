db86b2ec8f5ec97477c5138a03af95017a875f3c
pelino250 <pmutanguha@alueducation.com>
2024-10-30 15:29:16 +0200

    fix allowed host issues

--- gitpractice/settings.py
+++ gitpractice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#$tygsz@&z&8r=*i(5(10+26%%vff9x*s51st&!c-l($mgmty%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "159.223.98.21"]
 
 
 # Application definition
